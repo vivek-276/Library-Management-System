3a50755a21e3d1d23dd63202bef365c0163878c6
vivek-276 <rathodvivek025@gmail.com>
2024-08-11 18:59:42 +0530

    Add new test for borrow books

--- Library.test.js
+++ Library.test.js
@@ -1,16 +1,16 @@
 const Library = require('./Library');
 
-describe('Library Management System',()=>{
+describe('Library Management System', () => {
     let library;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         library = new Library();
     });
 
-    test('should add a book',()=>{
-        library.addBook('12345','The Alchemist','Paulo Coelho','1998');
+    test('should add a book', () => {
+        library.addBook('12345', 'The Alchemist', 'Paulo Coelho', '1998');
         const availableBooks = library.viewAvailableBooks();
-        expect(availableBooks).toEqual([{title: 'The Alchemist',author: 'Paulo Coelho',publicationYear:1998,available:true}]); 
+        expect(availableBooks).toEqual([{ title: 'The Alchemist', author: 'Paulo Coelho', publicationYear: 1998, available: true }]);
     });
 
     test('should borrow a book', () => {
@@ -19,4 +19,13 @@ describe('Library Management System',()=>{
         const availableBooks = library.viewAvailableBooks();
         expect(availableBooks).toEqual([]);
     });
+
+    test('should throw error when borrowing a book that is not available', () => {
+        library.addBook('1234567890', 'Book Title', 'Author Name', 2023);
+        library.borrowBook('1234567890');
+        expect(() => {
+            library.borrowBook('1234567890');
+        }).toThrow('Book is not available.');
+    });
+
 });
